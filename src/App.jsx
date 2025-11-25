c6e44964bbe1961f5f8a1db5a4b454e7108bd1f5
Abdulkaium007 <smakaium6957@gmail.com>
2025-11-25 23:16:49 +0600

    hi form kaium

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <>
       <h1 className="font-bold text-3xl">Welcome to BornoByte Portfolio</h1>
       <p className="mt-4 text-lg">
-        This is a sample portfolio website built with React and Tailwind CSS.
+        This is a sample portfolio website built with React and Tailwind CSS. kaium
       </p>
     </>
   );
