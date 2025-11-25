77f5a493db838c8dafa38ad4e43990b849e62ad9
Abdulkaium007 <smakaium6957@gmail.com>
2025-11-25 23:57:20 +0600

    mm

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="font-bold text-3xl">Welcome to BornoByte Portfolio kaium</h1>
+      <h1 className="font-bold text-3xl">Welcome to BornoByte Portfolio kaium 2</h1>
       <p className="text-lg">
         This is a sample portfolio website built with React and Tailwind CSS. kaium
       </p>
